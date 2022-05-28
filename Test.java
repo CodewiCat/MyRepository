b1e7359e6962d6bfd1f78f52dd7e068c3805436e
CodewiCat <106427804+CodewiCat@users.noreply.github.com>
2022-05-28 21:25:48 +0800

    Update Test.java
    08

--- Test.java
+++ Test.java
@@ -9,6 +9,7 @@ public class Test {
         System.out.println("Hello5");
         System.out.println("Hello6");
         System.out.println("Hello7");
+        System.out.println("Hello8");
     }
     public void speak(){
 
